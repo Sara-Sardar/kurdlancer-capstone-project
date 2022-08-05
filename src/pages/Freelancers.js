a47f4af52ca5b0fc4493034c04e75c0d71370b96
sara sardar <sarasardar2777@gmail.com>
2022-08-05 15:26:37 +0300

    feat: added hero section and body to jobs page

--- src/pages/Freelancers.js
+++ src/pages/Freelancers.js
@@ -3,8 +3,24 @@ import React from 'react'
 export default function Freelancers() {
   return (
     
-  <div className='m-6 text-white'>Freelancers
+  <div className='m-6  text-white bg-orange-200 '>Freelancers
         <div className='container mx-auto h-96  p-10 rounded-md  bg-amber-400 '>
+          <div className=' text-white md:flex flex space-x-96'>
+              <div className='p-10 m-10'>
+                    <p className=' text-4xl font-medium py-4'>For Clients,</p>
+                    <p className='text-xl '>Make it real with the best Freelancers, Hire a pro for any skills.</p>
+                </div>
+              
+
+                <div className="md:shrink-0">
+                  <img className="h-96 w-full object-cover md:h-full md:w-96"  src="https://images.unsplash.com/photo-1598257006458-087169a1f08d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="immage not loading"/>
+                </div>
+ 
+          </div>
+        </div>
+
+
+        <div className='container mx-auto h-screen p-10 rounded-md  bg-amber-400 mt-10'>
           <div className=' text-white md:flex flex space-x-96'>
               <div className='p-10 m-10'>
                     <p className=' text-4xl font-medium py-4'>For Clients,</p>
