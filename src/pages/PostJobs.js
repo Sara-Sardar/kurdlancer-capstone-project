71b9058a1ff3cecae68902d1c311414f408f0366
sara sardar <sarasardar2777@gmail.com>
2022-08-05 15:31:04 +0300

    feat: added hero section and body to postjobs page

--- src/pages/PostJobs.js
+++ src/pages/PostJobs.js
@@ -2,6 +2,45 @@ import React from 'react'
 
 export default function PostJobs() {
   return (
-    <div>PostJobs</div>
+    <div className='m-6  text-white bg-orange-200 '>PostJobs
+      <div className='m-6 text-white'> 
+                <div className='container mx-auto h-96  p-10 rounded-md  bg-amber-400'>
+                  <div className=' text-white md:flex flex space-x-96'>
+                        <div className='p-10 m-10'>
+                            <p className=' text-4xl font-medium py-4'>Post Jobs, </p>
+                            <p className='text-xl '>Publish Your Work To Find Best Freelancers,Post Your Work In IRK. </p>
+                        </div>
+                      
+
+                        <div className="md:shrink-0">
+                          <img className="h-96 w-full object-cover md:h-full md:w-96 rounded-md" src="https://images.unsplash.com/photo-1495521939206-a217db9df264?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="immage not loading"/>
+                        </div>
+
+                  </div>
+                </div>
+              </div>
+
+
+
+        <div className='container mx-auto h-screen p-10 rounded-md  bg-amber-400 mt-10'>
+          <div className=' text-white md:flex flex space-x-96'>
+              <div className='p-10 m-10'>
+                    <p className=' text-4xl font-medium py-4'>For Clients,</p>
+                    <p className='text-xl '>Make it real with the best Freelancers, Hire a pro for any skills.</p>
+                </div>
+              
+
+                <div className="md:shrink-0">
+                  <img className="h-96 w-full object-cover md:h-full md:w-96"  src="https://images.unsplash.com/photo-1598257006458-087169a1f08d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="immage not loading"/>
+                </div>
+
+          </div>
+        </div>
+
+
+
+
+
+    </div>
   )
 }
